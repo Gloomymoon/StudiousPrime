2fe917201efa1fadce66aa4ea5d12a0d3b072014
Gloomymoon <gloomymoon@gmail.com>
2016-12-11 15:23:09 +0800

    version 0.5

--- manage.py
+++ manage.py
@@ -17,7 +17,7 @@ def make_shell_context():
                 MyExercise=EnglishMyExercise, Book=EnglishBook, Setting=EnglishSetting)
 
 
-manager.add_command("runserver", Server(host="0.0.0.0", port=5000))
+manager.add_command("runserver", Server(host="0.0.0.0", port=(os.environ.get('PORT') or 5000)))
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("db", MigrateCommand)
 
